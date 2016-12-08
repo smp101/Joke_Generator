bf9ec19b83b973c9614c4f53a3d05b289f7c3ec6
Sabrina <smp101@francis.edu>
2016-12-08 18:35:20 -0500

    Joke section

---
+++ jokes.py
@@ -0,0 +1,25 @@
+# STARTED WITH JOKE GENERATOR V1.1
+# INITIALLY CREATED BY p0isonedpanda
+
+import random
+import os
+
+# This is our pool of jokes
+j1 = "Why did the chicken cross the road?"
+j2 = "To get to the idiot's house"
+j3 = "Knock Knock."
+j4 = "Who's there?"
+j5 = "The chicken."
+j6 = "What did the lawyer say to the other lawyer?"
+j7 = "We are both lawyers."
+j8 = "What did the farmer say when he lost his tractor?"
+j9 = "Where's my tractor?"
+j10 = "Why did the blonde get fired from the M&M factory?"
+j11 = "Repeated absence and stealing."
+j12 = "What do you call a priest that becomes a lawyer?"
+j13 = "A Father in law."
+j14 = "Knock Knock"
+j15 = "Who's there?"
+j16 = "Ho Ho."
+j17 = "Ho Ho Who?"
+j18 = "You know your Santa impression could use a little work..."
